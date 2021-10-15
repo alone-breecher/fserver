32445692d450f202cce2e2584df1a9d73aa35670
Anzil <76893049+alone-breecher@users.noreply.github.com>
2021-10-15 08:01:57 -0700

    Update fserv.sh

--- fserv.sh
+++ fserv.sh
@@ -2,6 +2,9 @@
 
 read -p "enter the domain: " dom
 
-echo "processing.........."
+echo "processing.....20%....."
+echo "processing......50%........"
+echo "processing........70%........."
+echo "processing..........100%..........."
 
 curl -v https://app.netlify.com/.netlify/functions/verify?domain=$dom 2>&1 | grep '"server":'
